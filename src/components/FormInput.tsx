aed8c24c5a9e4f275763d9b2f3deb081b729262b
alexgoni <goni000211@gmail.com>
2025-05-05 17:38:59 +0900

    feat: auth UI

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,29 @@
+interface Props {
+  type: string;
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+}
+
+export default function FormInput({
+  type,
+  placeholder,
+  required,
+  errors,
+}: Props) {
+  return (
+    <div className="flex flex-col gap-2">
+      <input
+        className="h-10 w-full rounded-md border-none bg-transparent ring-2 ring-neutral-200 transition placeholder:text-neutral-400 focus:ring-4 focus:ring-orange-500 focus:outline-none"
+        type={type}
+        placeholder={placeholder}
+        required={required}
+      />
+      {errors.map((error, index) => (
+        <span key={index} className="font-medium text-red-500">
+          {error}
+        </span>
+      ))}
+    </div>
+  );
+}
